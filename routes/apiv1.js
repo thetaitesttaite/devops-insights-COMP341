86afab102109f710e597f13f29e19d4c6eaf94f1
Taite Burrows <taite93@live.com>
2019-05-13 16:33:11 +0000

    idkwtd

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -7,7 +7,7 @@ var request = REQUEST.defaults( {
     strictSSL: false
 });
 
-//var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6cd2225a1dec81e05dc9dd27a611ad00";
+var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6cd2225a1dec81e05dc9dd27a611ad00";
 //var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/nz&appid=6cd2225a1dec81e05dc9dd27a611ad00";
 
 exports.getWeather = function(req, res) {
@@ -16,8 +16,8 @@ exports.getWeather = function(req, res) {
 		return res.status(400).send('zip missing');
 	}
 
-//	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
-var aurl = "https://api.openweathermap.org/data/2.5/weather?zip=" +zip+ ",us&appid=6cd2225a1dec81e05dc9dd27a611ad00";
+	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+//var aurl = "https://api.openweathermap.org/data/2.5/weather?zip=" +zip+ ",us&appid=6cd2225a1dec81e05dc9dd27a611ad00";
 	request({
 		method: 'GET',
         url: aurl,
--- static/js/controllers.js
+++ static/js/controllers.js
@@ -50,6 +50,26 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
                 } 
             });
         }
+//        else {
+//            $http({
+//                method: "GET",
+//                url: '/api/v1/getWeather?zip=' + data
+//            }).then( function(response) {
+//                if(which === 1) {
+//                    $scope.zip1City = response.data.city;
+//                    $scope.zip1Weather = response.data.weather;
+//                } else if(which === 2) {
+//                    $scope.zip2City = response.data.city;
+//                    $scope.zip2Weather = response.data.weather;
+//                } else if(which === 3) {
+//                    $scope.zip3City = response.data.city;
+//                    $scope.zip3Weather = response.data.weather;
+//                } else if(which === 4) {
+//                    $scope.zip4City = response.data.city;
+//                    $scope.zip4Weather = response.data.weather;
+//                } 
+//            });
+//        }
 
            else {
             if(which === 1) {
